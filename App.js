61d0ba4ea80ec77ee7da9a48c7a8c7938ec79789
Guillermo Echague <guillermo.echaguea@gmail.com>
2021-03-25 17:40:37 -0300

    Desarrollo APP Clima

---
+++ App.js
@@ -0,0 +1,110 @@
+
+import React, {useState, useEffect} from 'react';
+
+import { Keyboard,StyleSheet, View, TouchableWithoutFeedback, Alert} from 'react-native';
+
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+
+const App = () => {
+ 
+  const [ busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais:''
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [bgcolor, guardarBgcolor] = useState('rgb(71,149,212)');
+
+
+  const {ciudad, pais} = busqueda;
+
+  useEffect(()=>{
+   const consultarClima = async () => {
+    if(consultar) {
+      const appId = 'a8fd8fe260713ff930b7f7e65ebf2b62'
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+
+      try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+      
+        guardarResultado(resultado);
+        guardarConsultar(false);
+
+        // Modifica los colores de fondo basado en la temperatura
+        const kelvin = 273.15;
+        const {main} = resultado;
+        const actual = main.temp - kelvin;
+
+        if(actual < 10) {
+          guardarBgcolor('rgb(105, 108, 149)');
+        } else if(actual >= 10 && actual < 25) {
+          guardarBgcolor('rgb(71,149,212)');
+        } else {
+          guardarBgcolor('rgb(178, 28, 61)');
+        }
+
+      } catch (error) {
+        mostrarAlerta();
+      }
+    }
+   }
+   consultarClima();
+  }, [consultar]);
+
+  const mostrarAlerta = () => {
+    Alert.alert(
+        'Error',
+        'No hay resultados, intenta con otra ciudad o país',
+        [{text: 'OK'}]
+    )
+}
+
+  const ocultarTeclado = () => {
+    Keyboard.dismiss();
+  }
+
+  const bgColorApp = {
+    backgroundColor: bgcolor
+  }
+
+
+  return (
+    <>
+        <TouchableWithoutFeedback onPress={()=> ocultarTeclado()}>
+          <View style={[styles.app, bgColorApp]}>
+            <View style={styles.contenido}>
+              <Clima
+              resultado={resultado}
+              />
+              <Formulario
+              busqueda={busqueda} 
+              guardarBusqueda={guardarBusqueda}
+              guardarConsultar={guardarConsultar}
+              />
+            </View>
+          </View>
+       </TouchableWithoutFeedback>
+   
+    </>
+  );
+    
+ 
+};
+
+const styles = StyleSheet.create({
+  app: {
+    flex:1,
+    
+    justifyContent: 'center',
+  },
+  contenido: {
+    marginHorizontal: '2.5%',
+
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,67 @@
+# App de Clima
+
+APP con React Native consumiendo la API de Clima http://api.openweathermap.org/
+
+## Resultado de la APP
+
+**Inicio de la APP**
+
+![](img/inicio.png)
+
+**Validaciones**
+
+![](img/Error1.png) ![](img/Error2.png)
+
+**Paises**
+
+Estas peticiones se realizarón el dia 25-03-2021
+
+**Australia**
+
+![](img/Australia1.png) ![](img/Australia2.png)
+
+![](img/Australia3.png) ![](img/Australia4.png)
+
+![](img/Australia5.png) ![](img/Australia6.png)
+
+![](img/Australia7.png)
+
+**New Zeland**
+
+![](img/NZ1.png) ![](img/NZ2.png)
+
+![](img/NZ3.png)
+
+
+**España**
+
+![](img/Espana1.png) ![](img/Espana2.png)
+
+![](img/Espana3.png)
+
+
+**México**
+
+![](img/Mexico1.png) ![](img/Mexico2.png)
+
+![](img/Mexico3.png)
+
+**Perú**
+
+![](img/Peru1.png) ![](img/Peru2.png)
+
+![](img/Peru3.png)
+
+
+
+## Inicio Proyecto y Dependencias
+
+```
+npx react-native init climareact
+
+cd climareact
+
+npx react-native runa-andorid
+
+npm i @react-native-community/picker
+```
